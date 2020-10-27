a24514a351fc66b4a2da07e801770f4fa47ec26c
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-10-27 19:52:05 -0400

    added prompt for asking password length and also added validation for that

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// Assignment Code
+// // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+    var length = prompt("How many characters would you like your password to contain?");
+
+    if(length === null) {
+      return;
+    } else if (isNaN(length)) {
+      alert("It is not a number. Please enter any number.");
+    }
+    else if (length < 8 || length > 128) {
+      alert("Password length should be at least 8 characters and no more than 128 characters.");
+    } 
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +24,4 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
