8cefcf2829399f5f3ddc63bd350ce9c060bc32c4
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-10-27 20:06:26 -0400

    ask for confirming special, numeric, lowercase and uppercase characters and store its value in boolean variables

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
 
     var length = prompt("How many characters would you like your password to contain?");
+    var isSpecial, isNumeric, isLowercase, isUppercase;
 
     if(length === null) {
       return;
@@ -16,6 +17,39 @@ function generatePassword() {
     else if (length < 8 || length > 128) {
       alert("Password length should be at least 8 characters and no more than 128 characters.");
     } 
+
+    var special = confirm("Click ok to confirm including special characters");
+
+    if (special) {
+      isSpecial = true;
+    } else {
+      isSpecial = false;
+    }
+
+    var numeric = confirm("Click ok to confirm including numeric characters");
+
+    if (numeric) {
+      isNumeric = true;
+    } else {
+      isNumeric = false;
+    }
+
+    var lowercase = confirm("Click ok to confirm including lowercase characters");
+
+    if (lowercase) {
+      isLowercase = true;
+    } else {
+      isLowercase = false;
+    }
+
+    var uppercase = confirm("Click ok to confirm including uppercase characters");
+
+    if (uppercase) {
+      isUppercase = true;
+    } else {
+      isUppercase = false;
+    }
+
 }
 
 // Write password to the #password input
